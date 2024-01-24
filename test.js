48c8d943f383dba04d6d80216b88148c5f38f5e7
Mariana Barboza <46385012+Maribarboza@users.noreply.github.com>
2024-01-24 11:45:36 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+
+alert('Bem vindos!!!');
+let nome = 'test';
+let idade = 25;
+alert('Erro! Preencha todos os campos')
+let mensagemDeErro = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+nome = prompt('Nome do usuário?');
+idade = prompt('digite sua idade');
+if (idade <=18) {
+   
+    alert('Pode tirar Habilitação!');
+}
